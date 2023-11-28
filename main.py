57b74123ebda7b296a20b1cfc39cb05f7f42a46a
NaxximusPrime <143539630+NaxximusPrime@users.noreply.github.com>
2023-11-28 17:20:51 +0100

    Update main.py
    changed directory name to "user"

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 
-source_folder = r"C:\Users\eiste\Documents"
+source_folder = r"C:\Users\user\Documents"
 destination_folder = r"F:\Dokumente\zzz_moved_pdfs"
 
 print(source_folder)
 
-pdf_files = [file for file in os.listdir(r"C:\Users\eiste\Documents") if file.endswith(".pdf")]
+pdf_files = [file for file in os.listdir(r"C:\Users\user\Documents") if file.endswith(".pdf")]
 print(pdf_files)
 
 for file in pdf_files:
